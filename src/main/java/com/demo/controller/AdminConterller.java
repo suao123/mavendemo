79bd7f3f25fb68c670b27f903fd8e6b3e84a8f6b
suao123 <793544027@qq.com>
2019-05-10 11:00:55 +0800

    修改一系列错误

--- src/main/java/com/demo/controller/AdminConterller.java
+++ src/main/java/com/demo/controller/AdminConterller.java
@@ -1,4 +1,7 @@
 package com.demo.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class adminConterller {
 }
---
+++ src/main/java/com/demo/service/impl/StudioServiceimpl.java
@@ -0,0 +1,4 @@
+package com.demo.service.impl;
+
+public class StudioServiceimpl {
+}
